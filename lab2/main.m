4a463ef5dfe025d570d44c01b0fa41c042b52c44
renatoloureiro <renatoloureiro99@gmail.com>
2021-04-05 13:39:06 +0100

    few changes

--- lab2/main.m
+++ lab2/main.m
@@ -1,5 +1,5 @@
 % R1.a)
-M=512;
+M=2*512;
 w0=5.2*2*pi/M;
 samples=0:M-1;
 for n=1:length(samples)
@@ -11,14 +11,14 @@ end
 plot(samples,x);
 
 % R1.c)
-N=512;
+N=2*512;
 dfourier=fft(x);
 fs=1;
 f=fs*(0:N/2)/N;
 figure()
-plot(f,abs(dfourier(1:512/2 +1)/512))
+plot(f,abs(dfourier(1:M/2 +1)/M))
 figure()
-plot(f,angle(dfourier(1:512/2 +1)/512))
+plot(f,angle(dfourier(1:M/2 +1)/M))
 
 % R1.d)
 % determine the 3 maximum in the vector and its indices
@@ -27,7 +27,35 @@ max3=sortedv(1:3);
 ind3=indices(1:3);
 freq3=[f(ind3(1)),f(ind3(2)),f(ind3(3))];
 
+% recontruct the initial function from the fourier transform
+for i=1:M % x(i)
+   z(i)=0;
+    for j=1:N
+        if j== ind3(1) || j== ind3(2) || j== ind3(3) 
+            z(i)= z(i) + dfourier(j)*exp(1i*2*pi*j*i/N);   
+        end
+    end
+end
+% o problema parace ser o ganho/escala
 
+
+% R1.e)
+% just do a plot
+
+% R1.f)
+% do with N=1024
+
+
+% ---------------------------------------
+% R2.a)
+[p,Fs]=audioread('How_many_roads.wav');
+% soundsc(p,Fs);
+
+% R2.b) % get segment from sample 48500
+seg=p(48500:48500+2048-1);
+
+
+% R2.c)
 
 
 
