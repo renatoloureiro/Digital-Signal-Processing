c3acaa3ecf922a74578645c2f19aa4dec871b38c
Miguel Félix <mgfelix@academiafa.edu.pt>
2021-05-20 16:50:35 +0100

    da uma vista de olhos nas dimensoes, esta a dar um erro (R1e). plots adicionados

--- lab4/main.m
+++ lab4/main.m
@@ -10,21 +10,33 @@ a = r1a(x_train,N);
 %Prediction
 
 i=N+1;
-aux=x_train(1:N);
+x_predicted1=x_train(1:N);
 while(i<=length(x_train))
-   aux(i)=a*x_train(i-N);
+   x_predicted1(i)=a*x_train(i-N);
    i=i+1;
 end
 
-%Plot da training data, da prediction e do residuo correspondente
-
-%% R1c)
 %Calculo do residuo
 i=1;
 while(i<length(x_train)-N)
    r(i)=x_train(i+N)-a*x_train(i);
    i=i+1; 
 end
+
+%Plot da training data, da prediction e do residuo correspondente
+
+plot(x_train)
+title('Training Data')
+figure;
+plot(r)
+title('Resíduo r(n)')
+figure;
+plot(x_train)
+hold on
+plot(x_predicted1)
+legend('Training Data','Previsão')
+
+%% R1c)
 
 % Energia do residuo
 energy_r = energy(r);
@@ -47,6 +59,18 @@ r_predicted = H*a_r;
 
 %Calculo do residuo
 e = r' - r_predicted;
+
+% plot(e)
+% title('Resíduo e(n)')
+% figure;
+
+x_predicted2 = x_predicted1 + r_predicted;
+
+plot(x_train)
+hold on
+plot(x_predicted2)
+legend('Training Data','Prediction')
+figure;
 
 %% R1f)
 
