d8affbea3dfc508bb123ce3786a133c4328042b4
Miguel Félix <mgfelix@academiafa.edu.pt>
2021-05-21 17:23:30 +0100

    added plots for 1

--- lab4/main.m
+++ lab4/main.m
@@ -1,3 +1,6 @@
+clc;
+clear;
+
 load('energy_train.mat');
 N=96;
 
@@ -12,18 +15,34 @@ a = r1a(x_train,N);
 %i=N+1;
 %aux=x_train(1:N);
 for i=1:length(x_train)-N
-   aux(i)=a*x_train(i); 
+   x_predicted(i)=a*x_train(i); 
 end
 
-%Plot da training data, da prediction e do residuo correspondente
-
-%% R1c)
 %Calculo do residuo
 i=1;
 while(i<=length(x_train)-N)
    r(i)=x_train(i+N)-a*x_train(i);
    i=i+1; 
 end
+
+%Plot da training data, da prediction e do residuo correspondente
+
+figure();
+plot(x_train);
+hold();
+plot(N+1:length(x_train),x_predicted);
+xlabel("Amostras");
+ylabel("Amplitude");
+legend("Training Data (x\_train(t))", "Previsão (x\_predicted)");
+
+figure();
+plot(r);
+xlabel("Amostras");
+ylabel("Amplitude");
+legend("Resíduo (r(t))");
+
+%% R1c)
+
 
 % Energia do residuo
 energy_r = energy(r);
@@ -48,12 +67,26 @@ for n=1:length(r)-P
     end
 end
 
-x_new_predicted=aux(P+1:length(aux)) + r_predicted;
+x_new_predicted=x_predicted(P+1:length(x_predicted)) + r_predicted;
 
 
 %Calculo do residuo
 
 e=x_train(N+P+1:length(x_train))-x_new_predicted';
+
+figure();
+plot(x_train);
+hold();
+plot(N+1+P:length(x_train),x_new_predicted);
+xlabel("Amostras");
+ylabel("Amplitude");
+legend("Training Data (x\_train(t))", "Nova Previsão (x\_new\_predicted)");
+
+figure();
+plot(e);
+xlabel("Amostras");
+ylabel("Amplitude");
+legend("Resíduo (e(t))");
 
 %% R1f)
 
